d22edb6dae8217c424a7fc6bcda872c7d1080deb
Thnnathat Chaiphutha <thnnathatchaiphutha@gmail.com>
2023-03-06 21:37:16 +0700

    Sample data

--- SubProject/main.py
+++ SubProject/main.py
@@ -1,10 +1,15 @@
 import ikea_api
-
+import json;
 # Constants like country, language, base url
 constants = ikea_api.Constants(country="us", language="en")
 # Search API
 search = ikea_api.Search(constants)
 # Search endpoint with prepared data
-endpoint = search.search("Billy")
+endpoint = search.search("chair", limit=1)
 
-ikea_api.run(endpoint)
+data = ikea_api.run(endpoint)
+print()
+
+f = open("data.json", "a")
+f.write(json.dumps(data))
+f.close()
