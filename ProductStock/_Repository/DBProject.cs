187463d55a98057b455d98ab785fdb94dd9a5d1f
Thnnathat Chaiphutha <thnnathatchaiphutha@gmail.com>
2023-03-12 17:26:14 +0700

    DB Disconnect Except

--- ProductStock/_Repository/DBProject.cs
+++ ProductStock/_Repository/DBProject.cs
@@ -20,58 +20,71 @@ namespace ProductStock._Repository
         {
             string password = "thnnathat";
             string sql = "datasource=localhost;port=3306;username=root;password=" + password + ";database=product_stock";
-            MySqlConnection conn = new MySqlConnection(sql);
 
             try
             {
+                MySqlConnection conn = new MySqlConnection(sql);
                 conn.Open();
+                return conn;
             }
-            catch (MySqlException ex)
+            catch
             {
-                MessageBox.Show("Connection: " + ex);
+                MessageBox.Show("Database is not connect.");
+                return null;
             }
-            return conn;
         }
 
         public EmployeeModel getAEmpolyee(string EmpID)
         {
-            MySqlConnection conn = GetConnection();
-            EmployeeModel AEmp = new EmployeeModel();
             try
             {
-                string sql = "select * from employees where id='" + EmpID + "';";
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-                MySqlDataReader reader = cmd.ExecuteReader();
-                while (reader.Read())
+                MySqlConnection conn = GetConnection();
+                EmployeeModel AEmp = new EmployeeModel();
+                try
                 {
-                    AEmp.EmpID = reader.GetString(0);
-                    AEmp.Password = reader.GetString(1);
-                    AEmp.Prefix = reader.GetString(2);
-                    AEmp.FirstName = reader.GetString(3);
-                    AEmp.LastName = reader.GetString(4);
-                    AEmp.Role = reader.GetString(5);
-                    AEmp.Image = reader.GetFieldValue<byte[]>(6);
-                    AEmp.DateTime = reader.GetString(7);
+                    string sql = "select * from employees where id='" + EmpID + "';";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        AEmp.EmpID = reader.GetString(0);
+                        AEmp.Password = reader.GetString(1);
+                        AEmp.Prefix = reader.GetString(2);
+                        AEmp.FirstName = reader.GetString(3);
+                        AEmp.LastName = reader.GetString(4);
+                        AEmp.Role = reader.GetString(5);
+                        AEmp.Image = reader.GetFieldValue<byte[]>(6);
+                        AEmp.DateTime = reader.GetString(7);
+                    }
                 }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                }
+                conn.Close();
+                return AEmp;
             }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.ToString());
-            }
-            conn.Close();
-            return AEmp;
+            catch { MessageBox.Show("Connect database failed."); };
+            return null;
         }
 
         public DataTable displayData(string query)
         {
             string sql = query;
             MySqlConnection conn = GetConnection();
-            MySqlCommand cmd = new MySqlCommand(sql, conn);
-            MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
-            DataTable prodDt = new DataTable();
-            adp.Fill(prodDt);
-            conn.Close();
-            return prodDt;
+            if (conn != null)
+            {
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+                DataTable prodDt = new DataTable();
+                adp.Fill(prodDt);
+                conn.Close();
+                return prodDt;
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public bool removeData(string table, string id)
@@ -145,7 +158,7 @@ namespace ProductStock._Repository
             }
         }
 
-        public bool empDetailRepo(string sql, string id, string password, string prefix,string first_name, string last_name, string role, byte[] image)
+        public bool empDetailRepo(string sql, string id, string password, string prefix, string first_name, string last_name, string role, byte[] image)
         {
             MySqlConnection conn = GetConnection();
             try
